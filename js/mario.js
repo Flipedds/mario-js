452497809a43cb55e94a64b824f5e7bbe9ac0663
Filipe André <flipeandre12@gmail.com>
2023-03-28 19:20:15 -0300

    Atualiza mario

---
+++ js/mario.js
@@ -0,0 +1,45 @@
+const mario = document.querySelector('.mario')
+        const pipe = document.querySelector('.pipe')
+        const audio = document.querySelector('audio')
+        const cloud = document.querySelector('.cloud')
+        const cloud2 = document.querySelector('.cloud2')
+        
+        const jump = () => {
+            audio.src= 'audios/mario-jump.mp3';
+            audio.play();
+            mario.classList.add('jump');
+            setTimeout(() => {
+            mario.classList.remove('jump'); 
+            }, 500);
+        }
+
+        const loop = setInterval(()=>{
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+            if(pipePosition <= 200 && pipePosition > 0 && marioPosition <80){
+                pipe.style.animation = 'none';
+                pipe.style.left = `${230}px`;
+
+                mario.style.animation = 'none';
+                mario.style.bottom = `${100}px`;
+                
+                cloud.style.animation = 'none';
+                cloud2.style.animation = 'none';
+
+                mario.src = 'img/death.png';
+                mario.style.width = '150px';
+                mario.style.marginLeft = '50px';
+                audio.src= './audios/mario-som.mp3';
+                audio.play();
+                clearInterval(loop);
+            }
+        },10);
+
+        document.addEventListener('keydown', jump);
+
+        document.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {              
+                
+            }
+        })
